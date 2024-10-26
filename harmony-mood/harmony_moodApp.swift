9c7ee4914599eabdd5d31991a46f1024d996da1f
mauleenn <mauleen.ndlovu@outlook.com>
2024-10-25 20:25:34 -0500

    Initial Commit

---
+++ harmony-mood/harmony_moodApp.swift
@@ -0,0 +1,17 @@
+//
+//  harmony_moodApp.swift
+//  harmony-mood
+//
+//  Created by Mauleen Ndlovu on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct harmony_moodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ harmony-moodTests/harmony_moodTests.swift
@@ -0,0 +1,17 @@
+//
+//  harmony_moodTests.swift
+//  harmony-moodTests
+//
+//  Created by Mauleen Ndlovu on 10/25/24.
+//
+
+import Testing
+@testable import harmony_mood
+
+struct harmony_moodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
